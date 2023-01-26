655b53a1bdc09aeb2ae8e455a2b68821c1efa8a7
Tanmay Sharma <sharmatanmay617@gmail.com>
2023-01-26 15:13:40 +0300

    Linter added

--- models/person.js
+++ models/person.js
@@ -1,40 +1,40 @@
 require('dotenv').config()
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+	.connect(url)
+	.then((result) => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true,
-    unique: true
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    required: true
-  },
-});
+	name: {
+		type: String,
+		minLength: 3,
+		required: true,
+		unique: true
+	},
+	number: {
+		type: String,
+		minLength: 8,
+		required: true
+	},
+})
 
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+personSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
